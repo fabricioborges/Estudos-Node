8f085a009c45a2558b02b615fa4a722803b16f6d
Fabricio Borges <fabricio.borges@ndd.com.br>
2019-12-30 18:01:01 -0300

    criado função de like e dislike

--- Estudos-Node/tindev/frontend/src/pages/main/Main.js
+++ Estudos-Node/tindev/frontend/src/pages/main/Main.js
@@ -22,11 +22,19 @@ export default function Main({ match }) {
     }, [match.params.id]);
 
     async function handleLike(id){
-        console.log('like', id);
+        await api.post(`/devs/${id}/likes`, null, {
+            headers: {user: match.params.id},
+        })
+
+        setUsers(users.filter(user => user._id !== id));
     }
     
     async function handleDislike(id){
-        console.log('dislike', id);
+        await api.post(`/devs/${id}/dislikes`, null, {
+            headers: {user: match.params.id},
+        })
+
+        setUsers(users.filter(user => user._id !== id));
     }
 
     return (
@@ -41,11 +49,11 @@ export default function Main({ match }) {
                             <p>{user.bio}</p>
                         </footer>
                         <div className="buttons">
-                            <button type="button">
-                                <img src={dislike} alt="dislike" onClick={() => handleDislike(user._id)}/>
+                            <button type="button" onClick={() => handleDislike(user._id)}>
+                                <img src={dislike} alt="dislike" />
                             </button>
-                            <button type="button">
-                                <img src={like} alt="like" onClick={() => handleLike(user._id)}/>
+                            <button type="button" onClick={() => handleLike(user._id)}>
+                                <img src={like} alt="like" />
                             </button>
                         </div>
                     </li>
